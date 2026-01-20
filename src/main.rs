416f68de33b2bd77ccc00023a712f3799e4fd562
Lilac Bogus <253619263+RustyKernelPunk@users.noreply.github.com>
2026-01-19 18:26:40 -0700

    Final touches pt. 2

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,9 @@ use chacha20poly1305::{
 };
 use rpassword::read_password;
 use color_eyre::eyre::{Result, bail, eyre};
-
 use rand_core::{OsRng, TryRngCore};
+
+//Arguments for clap
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -30,36 +31,47 @@ struct Args {
     #[arg(short, long, default_value = "note.enc")]
     filepath: String,
   }
+//Constants for decryption
 const SALT_LENGTH: usize = 16;
 const NONCE: usize = 12;
 fn main() -> Result<()>{
+    //Setup color eyre for pretty errors
     color_eyre::install()?;
 
+    //Parse args, setup path for quikcrypt
     let args = Args::parse();
     let mut path: PathBuf = dirs::home_dir().expect("Failed to get home directory.");
     path.push(".config");
     path.push("quikcrypt");
-    if path.exists(){
-        println!("~/.config/quikcrypt exists, moving on...");
-    } else{
-        println!("Creating ~/.config/quikcrypt directory for encrypted storage...");
-        fs::create_dir_all(&path)?;
-    }
     if args.create_file{
+        //Checks if path exists, creates if not
+        if path.exists(){
+            println!("~/.config/quikcrypt exists, moving on...");
+        } else{
+            println!("Creating ~/.config/quikcrypt directory for encrypted storage...");
+            fs::create_dir_all(&path)?;
+        }
+        
+        //Setups for byte values
         let mut key = [0u8; 32];
         let mut salt = [0u8; 16];
         let mut nonce_bytes = [0u8; 12];
 
+        //Plaintext piping
         let mut plaintext = Vec::new();
         std::io::stdin().read_to_end(&mut plaintext)?;
 
+        //Uses rpassword for secure password entering
         println!("Enter a secure encryption password: ");
         let password = read_password()?;
         let password = password.as_bytes();
 
+        //Failsafe to prevent short passwords
         if password.len() < 8 {
             bail!("Password too short and insecure! Must be longer!");
         }
+        
+        //Rng setup, key setup, nonce setup, cipher setup, writes to encrypted file
         OsRng.try_fill_bytes(&mut salt).unwrap();
         Argon2::default().hash_password_into(password, &salt[..], &mut key).expect("Couldn't hash password!");        
         OsRng.try_fill_bytes(&mut nonce_bytes).unwrap();
@@ -75,37 +87,50 @@ fn main() -> Result<()>{
 
         println!("Creating file at ~/.config/quikcrypt/{}", args.filepath);
 
+        //Zeroizes for security
         plaintext.zeroize();
         key.zeroize();
         let mut password_bytes = password.to_vec();
         password_bytes.zeroize();
     }
     if args.decrypt{
+        //Bails because it cannot decrypt
+        if !path.exists(){
+            bail!("Can't decrypt a file from a non-existent ~/.config directory.");
+        }
+
+        //Key bytes, password entering
         let mut key = [0u8; 32];
         println!("Enter the file's password: ");
         let password = read_password()?;
         let password = password.as_bytes();
-
+        
+        //Pushes file name
         path.push(args.filepath);
 
+        //Opens path
         let mut encrypted_file = File::open(path)?;
         let mut buf = Vec::new();
         encrypted_file.read_to_end(&mut buf)?;
         
+        //Reads salt, nonce, ciphertext
         let salt = buf[0..SALT_LENGTH].to_vec();
         let nonce_slice = buf[SALT_LENGTH..SALT_LENGTH + NONCE].to_vec();
         let ciphertext = buf[SALT_LENGTH + NONCE..].to_vec();
         
+        //Decrypts
         println!("Decrypting file...");
         Argon2::default().hash_password_into(password, &salt, &mut key).expect("Failed to hash password!");
         let cipher = ChaCha20Poly1305::new(&key.into());
         let nonce = Nonce::from_slice(&nonce_slice);
         let plaintext = cipher.decrypt(nonce, ciphertext.as_ref()).map_err(|_| eyre!("Decryption failed!"))?;
         
+        //Pipes to stdout
         let mut out = io::stdout();
         out.write_all(&plaintext)?;
         out.flush()?;
     
+        //Zeroizes for security
         key.zeroize();
         let mut password_bytes = password.to_vec();
         password_bytes.zeroize();
